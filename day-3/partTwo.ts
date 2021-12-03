a8f6319e6295b95b9ea7f17492b40d465447b0fd
justingodden <justingodden@gmail.com>
2021-12-03 07:09:23 +0000

    day 3 python

---
+++ day-3/partTwo.ts
@@ -0,0 +1,29 @@
+// npx ts-node ./day-3/partTwo.ts
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+const data: string = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+
+const strArr: string[] = data.split('\r\n')
+
+let x: number = 0
+let y: number = 0
+
+strArr.forEach((str, idx) => {
+    let direction: string = str.replace(/[^a-zA-Z]/g, '')
+    let value: number = parseInt(str.replace(/[^0-9]/g, ''))
+    switch (direction) {
+        case "forward":
+            x += value
+            break;
+        case "down":
+            y += value
+            break;
+        case "up":
+            y -= value
+            break;
+    }
+})
+
+console.log(x * y)
