8344d92c6687c8bf05aedf81fbc5e71d3ad2f9a3
justingodden <justingodden@gmail.com>
2021-12-21 11:08:41 +0000

    refactor(day-21): add return types and clean up some code

--- day-21/partTwo.ts
+++ day-21/partTwo.ts
@@ -8,30 +8,30 @@ interface Player {
     score: number
 }
 
-const singleStep = (player: Player, diceOutcome: number) => {
+const singleStep = (player: Player, diceOutcome: number): void => {
     player.pos = ((player.pos - 1 + diceOutcome) % 10) + 1
     player.score += player.pos
 }
 
-function newUniverse(p1: Player, p2: Player, p1Turn: boolean) {
+const newUniverse = (p1: Player, p2: Player, p1Turn: boolean): number => {
     if (p1.score >= 21) {
         return 1;
     } else if (p2.score >= 21) {
         return 0;
     }
 
-    const currPlayer = p1Turn ? p1 : p2;
-    let sum = 0;
+    const currPlayer = p1Turn ? p1 : p2
+    let sum = 0
     for (let diceOutcome = 3; diceOutcome <= 9; diceOutcome++) {
-        const oldPos = currPlayer.pos;
-        const oldScore = currPlayer.score;
-        singleStep(currPlayer, diceOutcome);
-        const multiplier = outcomes[diceOutcome];
-        sum += multiplier * newUniverse(p1, p2, !p1Turn);
-        currPlayer.pos = oldPos;
-        currPlayer.score = oldScore;
+        const oldPos = currPlayer.pos
+        const oldScore = currPlayer.score
+        singleStep(currPlayer, diceOutcome)
+        const multiplier = outcomes[diceOutcome]
+        sum += multiplier * newUniverse(p1, p2, !p1Turn)
+        currPlayer.pos = oldPos
+        currPlayer.score = oldScore
     }
-    return sum;
+    return sum
 }
 
 let p1: Player = {
